727a4352bbc2b3819ff0e0a066f5665b8948f755
senthilsk2003 <senthilkumarofficial2003@gmail.com>
2023-11-19 02:54:55 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Header from './Header'; //Include Header
+import Footer from './Footer';
+import Home from './Home'
+function App() {
+  return (
+    <div classNamnpme="App">
+      
+       
+          <Header></Header>
+           <Home></Home>
+          <Footer></Footer>
+        
+     
+    </div>
+  );
+}
+
+export default App;
